4d049f2956d434c01bb39df0a02c8790b6b30aed
candelo09 <qualitycolombia.dvp05@gmail.com>
2025-01-31 18:02:56 -0500

    upgrate version 17 and core membership

---
+++ src/app/memberships/membership-form/membership-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membership-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './membership-form.component.html',
+  styleUrl: './membership-form.component.css'
+})
+export class MembershipFormComponent {
+
+}
---
+++ src/app/memberships/membership-list/membership-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membership-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './membership-list.component.html',
+  styleUrl: './membership-list.component.css'
+})
+export class MembershipListComponent {
+
+}
